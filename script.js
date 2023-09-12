e201f1553ca246a9d2f6aeed22b29a411307b8d0
Onah Patience Tochukwu <65729360+tchytech@users.noreply.github.com>
2023-09-12 15:54:36 +0100

    done

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     function getCurrentDayOfWeek() {
-      const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      const currentDate = new Date();
-      const dayOfWeek = daysOfWeek[currentDate.getDay()];
-      return dayOfWeek;
+        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const currentDate = new Date();
+        const dayOfWeek = daysOfWeek[currentDate.getDay()];
+        return dayOfWeek;
     }
 
-    function getCurrentUTCTime() {
-      const currentTime = new Date();
-      const hours = currentTime.getUTCHours().toString().padStart(2, '0');
-      const minutes = currentTime.getUTCMinutes().toString().padStart(2, '0'); 
-      const seconds = currentTime.getUTCSeconds().toString().padStart(2, '0'); 
-      return `${hours}:${minutes}:${seconds}`;
+    function updateUTCTime() {
+        const currentTime = new Date();
+        const hours = currentTime.getUTCHours().toString().padStart(2, '0');
+        const minutes = currentTime.getUTCMinutes().toString().padStart(2, '0');
+        const seconds = currentTime.getUTCSeconds().toString().padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+        document.querySelector('[data-testid="currentUTCTime"]').textContent = timeString;
     }
-  
+
+    updateUTCTime();
+
+    setInterval(updateUTCTime, 1000);
 
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = getCurrentDayOfWeek();
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = getCurrentUTCTime();
 });
